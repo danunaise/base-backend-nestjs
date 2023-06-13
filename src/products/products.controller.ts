3009820ed614d9849251adcc575478a189cf70cd
danunnaise <danunai1993@gmail.com>
2023-06-13 16:11:31 +0700

    add protection in product route for admin

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,15 +7,22 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductDto } from './product.dto';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/roles/role.guard';
+import { RoleName } from '@prisma/client';
+import { Roles } from 'src/auth/roles/role.decorator';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post('create')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(RoleName.ADMIN)
   async create(@Body() dto: ProductDto) {
     return this.productsService.createProduct(dto);
   }
@@ -26,11 +33,15 @@ export class ProductsController {
   }
 
   @Put('edit/:id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(RoleName.ADMIN)
   async update(@Param('id', ParseIntPipe) id: number, @Body() data: any) {
     return this.productsService.updateProduct(id, data);
   }
 
   @Delete('delete/:id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(RoleName.ADMIN)
   async delete(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.deleteProduct(id);
   }
